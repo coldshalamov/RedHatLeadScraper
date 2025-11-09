660174c545b5762eefa35f7d487b75267fd46360
coldshalamov <93robingattis@gmail.com>
2025-11-09 12:36:42 -0500

    Add FastPeopleSearch scraper and CLI harness

---
+++ lead_verifier/__init__.py
@@ -0,0 +1,9 @@
+"""Core package for lead verification utilities."""
+
+from .models import LeadInput, LeadResult, PhoneNumberResult
+
+__all__ = [
+    "LeadInput",
+    "LeadResult",
+    "PhoneNumberResult",
+]
